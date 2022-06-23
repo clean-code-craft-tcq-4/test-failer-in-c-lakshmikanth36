7ab3537c197d25c0573a1c69b4c29ae6d5f32551
Lakshmikanth Routhu <lakshmikanthr36@gmail.com>
2022-06-23 11:11:49 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <assert.h>
 #define numberOfMajorMinorColors 5
+#define COLORMAPVALUE1 24
+#define COLORMAPVALUE2 25
+#define COLORMAPVALUE3 26
 int printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     int majorColorIndex = 0, minorColorIndex = 0;
     for(majorColorIndex = 0; majorColorIndex < numberOfMajorMinorColors; majorColorIndex++) {
         for(minorColorIndex = 0; minorColorIndex < numberOfMajorMinorColors; minorColorIndex++) {
-            printf("%d | %s | %s\n", majorColorIndex * 5 + minorColorIndex, majorColor[majorColorIndex], minorColor[minorColorIndex]);
+            printf("%d | %s | %s\n", (majorColorIndex * numberOfMajorMinorColors + minorColorIndex), majorColor[majorColorIndex], minorColor[minorColorIndex]);
         }
     }
     return  majorColorIndex * minorColorIndex;
@@ -15,9 +18,9 @@ int printColorMap() {
 
 int main() {
     int result = printColorMap();
-    assert(result == 24);
-    assert(result == 25);
-    assert(result == 26);
+    assert(result == COLORMAPVALUE1);
+    assert(result == COLORMAPVALUE2);
+    assert(result == COLORMAPVALUE3);
     printf("All is well (maybe!)\n");
     return 0;
 }
