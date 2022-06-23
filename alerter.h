8de62a1ef9cead3274fa5c554c3b14be16568303
Lakshmikanth Routhu <lakshmikanthr36@gmail.com>
2022-06-23 10:50:47 +0530

    Create alerter.h

---
+++ alerter.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <assert.h>
+#define THERSHOLD_STUB_VALUE 500
+#define MAX_STUB_VALUE 200
+#define FRENHEIT_VALUE1 400.5
+#define FRENHEIT_VALUE2 303.6
+#define TRUE 0
+#define FALSE 1
+int alertFailureCount = 0;
+int networkAlertStub(float celcius);
+void alertInCelcius(float farenheit);
