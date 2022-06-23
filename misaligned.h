e498769a3fc8eb4868c3bd3b164f3d94fe985693
Lakshmikanth Routhu <lakshmikanthr36@gmail.com>
2022-06-23 11:21:51 +0530

    Create misaligned.h

---
+++ misaligned.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <assert.h>
+#define numberOfMajorMinorColors 5
+#define COLORMAPVALUE1 24
+#define COLORMAPVALUE2 25
+#define COLORMAPVALUE3 26
+int printColorMap();
+void testprintColorMap();
