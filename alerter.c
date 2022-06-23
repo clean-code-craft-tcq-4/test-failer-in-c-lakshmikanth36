c6aef0c96471d9b66e09d7b70d2fb05c0e441b25
Lakshmikanth Routhu <lakshmikanthr36@gmail.com>
2022-06-23 10:51:42 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -1,12 +1,4 @@
-#include <stdio.h>
-#include <assert.h>
-#define THERSHOLD_STUB_VALUE 500
-#define MAX_STUB_VALUE 200
-#define FRENHEIT_VALUE1 400.5
-#define FRENHEIT_VALUE2 303.6
-#define TRUE 0
-#define FALSE 1
-int alertFailureCount = 0;
+#include "alerter.h"
 
 int networkAlertStub(float celcius) {
     printf("ALERT: Temperature is %.1f celcius.\n", celcius);
