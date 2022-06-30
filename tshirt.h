b526bb4112ac0589c49e4401bf5d55b7f20e4ce2
Lakshmikanth Routhu <lakshmikanthr36@gmail.com>
2022-06-30 07:46:54 +0530

    Update tshirt.h

--- tshirt.h
+++ tshirt.h
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <assert.h>
 
-#define TSHIRTSIZE_S 38
-#define TSHIRTSIZE_L 42
+#define TSHIRTSIZE_S1  37
+#define TSHIRTSIZE_S2  38
+#define TSHIRTSIZE_M1  40
+#define TSHIRTSIZE_M2  42
+#define TSHIRTSIZE_L   43
 
 char size(int cms);
 void testtShirt();
