04412e7f19959a8b385c239f7ce5960fc2f2c553
Lakshmikanth Routhu <lakshmikanthr36@gmail.com>
2022-06-30 07:59:00 +0530

    Update misaligned.h

--- misaligned.h
+++ misaligned.h
@@ -4,5 +4,9 @@
 #define COLORMAPVALUE1 24
 #define COLORMAPVALUE2 25
 #define COLORMAPVALUE3 26
+const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+int majorColorIndex = 0, minorColorIndex = 0;
 int printColorMap();
 void testprintColorMap();
+int GetprintColorMap();
