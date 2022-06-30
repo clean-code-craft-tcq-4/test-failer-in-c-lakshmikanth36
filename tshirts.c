70f2e0bebb3c64b6852844ee4339c8d83f737d34
Lakshmikanth Routhu <lakshmikanthr36@gmail.com>
2022-06-30 07:48:08 +0530

    Update tshirts.c

--- tshirts.c
+++ tshirts.c
@@ -1,11 +1,11 @@
 #include "tshirt.h"
 char size(int cms) {
-    char sizeName = '\0';
-    if(cms <=TSHIRTSIZE_S) {
+
+    if(cms <=TSHIRTSIZE_S2) {
         sizeName = 'S';
-    } else if(cms > TSHIRTSIZE_S && cms <= TSHIRTSIZE_L) {
+    } else if(cms > TSHIRTSIZE_S2 && cms <=TSHIRTSIZE_M2) {
         sizeName = 'M';
-    } else if(cms > TSHIRTSIZE_L) {
+    } else if(cms > TSHIRTSIZE_M2) {
         sizeName = 'L';
     }
     return sizeName;
@@ -13,8 +13,11 @@ char size(int cms) {
 
 void testTShirt()
 {
-    assert(size(TSHIRTSIZE_S) == 'S');
-    assert(size(TSHIRTSIZE_L) == 'M');
+    assert(size(TSHIRTSIZE_S1) == 'S');
+    assert(size(TSHIRTSIZE_S1) == 'S');
+    assert(size(TSHIRTSIZE_M1) == 'M');
+    assert(size(TSHIRTSIZE_M2) == 'M');
+    assert(size(TSHIRTSIZE_L) == 'L');
     printf("\nAll is well (maybe!)\n");
 }
 
