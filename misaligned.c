fe78975447e29619e0b7a3f0043957579973970d
Lakshmikanth Routhu <lakshmikanthr36@gmail.com>
2022-06-30 07:55:01 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -1,22 +1,29 @@
 #include "misaligned.h"
 
+int GetprintColorMap()
+{
+    return (majorColorIndex * numberOfMajorMinorColors + minorColorIndex+1);
+}
+
 int printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     int majorColorIndex = 0, minorColorIndex = 0;
     for(majorColorIndex = 0; majorColorIndex < numberOfMajorMinorColors; majorColorIndex++) {
         for(minorColorIndex = 0; minorColorIndex < numberOfMajorMinorColors; minorColorIndex++) {
-            printf("%d | %s | %s\n", (majorColorIndex * numberOfMajorMinorColors + minorColorIndex+1), majorColor[majorColorIndex], minorColor[minorColorIndex]);
+            printf("%d | %s | %s\n",GetprintColorMap(), majorColor[majorColorIndex], minorColor[minorColorIndex]);
         }
     }
     return  majorColorIndex * minorColorIndex;
 }
+
 void testprintColorMap()
 {
     int result = printColorMap();
     assert(result == 25);
     printf("All is well (maybe!)\n");
 }
+
 int main() {
     testprintColorMap();
     return 0;
