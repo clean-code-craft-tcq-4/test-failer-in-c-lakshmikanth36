1dd068bb9b01867d8bd963d45929c73dc8158b36
Lakshmikanth Routhu <lakshmikanthr36@gmail.com>
2022-06-30 08:28:51 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -26,11 +26,9 @@ int alertInCelcius(float farenheit) {
 
 void testalertInCelcius()
 {
-    //alertInCelcius(FRENHEIT_VALUE1);
-    //assert(alertFailureCount == FALSE);
-    assert(alertInCelcius(303.6)==0);
-    assert(alertInCelcius(400.5)==1);
-    assert(alertInCelcius(500.5)==2);
+    assert(alertInCelcius(FRENHEIT_VALUE1)==0);
+    assert(alertInCelcius(FRENHEIT_VALUE2)==1);
+    assert(alertInCelcius(FRENHEIT_VALUE3)==2);
     printf("%d alerts failed.\n", alertFailureCount);
     printf("All is well (maybe!)\n");
 }
