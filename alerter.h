c4bf8c20f3f666ebfef004123cfa6351b9f733b2
Lakshmikanth Routhu <lakshmikanthr36@gmail.com>
2022-06-30 08:28:45 +0530

    Update alerter.h

--- alerter.h
+++ alerter.h
@@ -2,8 +2,9 @@
 #include <assert.h>
 #define THERSHOLD_STUB_VALUE 500
 #define MAX_STUB_VALUE 200
-#define FRENHEIT_VALUE1 400.5
-#define FRENHEIT_VALUE2 303.6
+#define FRENHEIT_VALUE1 303.6
+#define FRENHEIT_VALUE2 400.5
+#define FRENHEIT_VALUE3 500.5
 #define TRUE 0
 #define FALSE 1
 int alertFailureCount = 0;
